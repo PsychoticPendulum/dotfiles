aa64bfd4dfab7c0a3ea87fe7566ea258c6fa1a29
psychicpenguin <lukas.mairock@protonmail.com>
2022-06-21 21:53:32 +0200

    Added polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,17 @@
+#!usr/bin/env bash
+
+killall -q polybar
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload bar1 &
+	done
+else
+	polybar --reload bar1 &
+fi
+
+##  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 >> /tmp/polybar1.log 2>&1 &
+# polybar bar2 >> /tmp/polybar2.log 2>&1 &
+
+echo "Bars launched ..."
