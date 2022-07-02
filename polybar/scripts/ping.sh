047f4cd795153841f343a4b8a4be9f2c1a734d44
psychicpenguin <lukas.mairock@protonmail.com>
2022-07-02 14:15:49 +0200

    New colors

--- polybar/scripts/ping.sh
+++ polybar/scripts/ping.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-ping -qc1 google.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? ""$5"":"DOWN") }'
+PING=$(ping -qc1 google.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? ""$5"":"DOWN") }')
+
+BASE=${PING::-4}
+COLOR="%{F#00ff00}"
+
+if [[ $BASE -gt 100 ]]; then
+	COLOR="%{F#ff0000}"
+elif [[ $BASE -gt 30 ]]; then
+	COLOR="%{F#ffff00}"
+fi
+
+echo $COLOR $PING
