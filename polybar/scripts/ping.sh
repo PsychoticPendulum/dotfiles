966def2209668188bb782f5330328d13faaaef39
psychicpenguin <lukas.mairock@protonmail.com>
2022-07-02 15:08:18 +0200

    New colors

--- polybar/scripts/ping.sh
+++ polybar/scripts/ping.sh
@@ -5,8 +5,12 @@ PING=$(ping -qc1 google.com 2>&1 | awk -F'/' 'END{ print (/^rtt/? ""$5"":"DOWN")
 BASE=${PING::-4}
 COLOR="%{F#00ff00}"
 
-if [[ $BASE -gt 100 ]]; then
+if [[ $BASE -eq "DOWN" ]]; then
 	COLOR="%{F#ff0000}"
+elif [[ $BASE -gt 200 ]]; then
+	COLOR="%{F#ff0000}"
+elif [[ $BASE -gt 100 ]]; then
+	COLOR="%{F#ff8800}"
 elif [[ $BASE -gt 30 ]]; then
 	COLOR="%{F#ffff00}"
 fi
