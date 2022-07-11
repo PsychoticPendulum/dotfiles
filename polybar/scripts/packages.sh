c3a272ea18ba0b78037428a7a1beb3b7a58edb6e
psychicpenguin <lukas.mairock@protonmail.com>
2022-07-11 20:35:17 +0200

    asthetic change

--- polybar/scripts/packages.sh
+++ polybar/scripts/packages.sh
@@ -4,8 +4,8 @@ TOTAL=$(pacman -Q | wc -l)
 PENDING=$(checkupdates 2> /dev/null | wc -l)
 COLOR=""
 if [[ "$PENDING" -gt "25" ]]; then
-	COLOR="-%{F#ff0000}$PENDING"
+	COLOR=" %{F#ff0000}$PENDING"
 elif [[ "$PENDING" -gt "0" ]]; then
-	COLOR="-%{F#ffff00}$PENDING"
+	COLOR=" %{F#ffff00}$PENDING"
 fi
 echo $TOTAL$COLOR
