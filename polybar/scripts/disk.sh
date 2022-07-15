dd8addaf0a7e01e091d5d5ed1f17ffc7741c4765
psychicpenguin <lukas.mairock@gmail.com>
2022-07-15 20:33:25 +0200

    added free space

--- polybar/scripts/disk.sh
+++ polybar/scripts/disk.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+TOTAL=$(df / -h | awk '{print $4}' | tail -n 1)
 FREE=$(df / -h | awk '{print $3}' | tail -n 1)
 PERC=$(df / -h | awk '{print $5}' | tail -n 1)
 PERC=${PERC::-1}
@@ -10,4 +11,4 @@ if [[ $PERC -gt 90 ]]; then
 elif [[ $PERC -gt 50 ]]; then
 	COLOR="%{F#ffff00}"
 fi
-echo $FREE $COLOR$PERC\%
+echo $FREE $TOTAL $COLOR$PERC\%
