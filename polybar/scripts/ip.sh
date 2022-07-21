7a970d7c8cba09abdd0c30b52b646ce16a738f41
psychicpenguin <lukas.mairock@gmail.com>
2022-07-21 23:07:48 +0200

    enp devices

--- polybar/scripts/ip.sh
+++ polybar/scripts/ip.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-IP=$(ip -4 -o addr show | grep tun || ip -4 -o addr show | grep eth || ip -4 -o addr show | grep wlan)  
+IP=$(ip -4 -o addr show | grep tun || ip -4 -o addr show | grep eth | grep enp | ip -4 -o addr show | grep wlan)  
 DEV=$(echo $IP | awk '{print $2}')
 IP=$(echo $IP | awk '{print $4}')
 echo -n "$DEV: "
