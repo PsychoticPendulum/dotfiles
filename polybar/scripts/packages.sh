581bb75bb48f7cae60b40bba474ecb8e3d9c60c4
psychicpenguin <lukas.mairock@gmail.com>
2022-08-05 21:11:23 +0200

    forgot to remove the hardcoded value I used for testing lmao

--- polybar/scripts/packages.sh
+++ polybar/scripts/packages.sh
@@ -4,8 +4,6 @@ TOTAL=$(pacman -Q | wc -l)
 PENDING=$(checkupdates 2> /dev/null | wc -l)
 COLOR=""
 
-PENDING=3
-
 if [[ "$PENDING" -gt "25" ]]; then
 	COLOR=" %{F#ff0000}$PENDING"
 elif [[ "$PENDING" -gt "10" ]]; then
