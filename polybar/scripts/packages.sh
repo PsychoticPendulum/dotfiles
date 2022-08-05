226b19884552e50fcb89fcec6d4e0397a20fc902
PsychoticPendulum <lukas.mairock@gmail.com>
2022-08-05 10:41:10 +0200

    colors

--- polybar/scripts/packages.sh
+++ polybar/scripts/packages.sh
@@ -3,9 +3,14 @@
 TOTAL=$(pacman -Q | wc -l)
 PENDING=$(checkupdates 2> /dev/null | wc -l)
 COLOR=""
+
+PENDING=3
+
 if [[ "$PENDING" -gt "25" ]]; then
 	COLOR=" %{F#ff0000}$PENDING"
-elif [[ "$PENDING" -gt "0" ]]; then
+elif [[ "$PENDING" -gt "10" ]]; then
 	COLOR=" %{F#ffff00}$PENDING"
+elif [[ "$PENDING" -gt "0" ]]; then
+	COLOR=" %{F#88ff00}$PENDING"
 fi
 echo $TOTAL$COLOR
