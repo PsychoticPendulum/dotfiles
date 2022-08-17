df69cb8e1c72d4e5012ede361a87c4ed28a87924
psychicpenguin <lukas.mairock@gmail.com>
2022-08-17 18:38:04 +0200

    rss

--- fish/config.fish
+++ fish/config.fish
@@ -119,6 +119,7 @@ alias mv="mv -v"
 alias ra="ranger"
 alias rh="ranger --cmd 'set show_hidden True'"
 alias nfetch="neofetch --ascii_distro Arch | lolcat -F 0.1 -a -s 5000 -S 174"
+alias rss="newsboat -r"
 
 # Locations
 alias cra="clear ; ra"
