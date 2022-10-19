d510dc587a41fee4d2cc15346192493476e6f923
luks <lukas.mairock@gmail.com>
2022-10-19 21:14:00 +0200

    kill all torrent programs if vpn is inactive

--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -3,6 +3,7 @@ echo -n 'VPN: '
 if ip addr | grep tun > /dev/null; then
 	echo '%{F#00ff00}Active'
 	exit
+else
+	echo '%{F#ff0000}Inactive'
+	ps -aux | grep 'torrent' | awk '{print $2}' | xargs kill
 fi
-echo '%{F#ff0000}Inactive'
-ps -aux | grep 'torrent' | awk '{print $2}' | xargs kill
