8902da599ca48cab6fdea3230185c0b39149fe15
luks <lukas.mairock@gmail.com>
2022-11-21 10:27:40 +0100

    better localhost

--- polybar/scripts/ip.sh
+++ polybar/scripts/ip.sh
@@ -1,16 +1,7 @@
 #! /bin/bash
 
-IP=$(ip -4 -o addr show | grep tun || ip -4 -o addr show | grep eth || ip -4 -o addr show | grep enp || ip -4 -o addr show | grep wlan)  
+IP=$(ip -4 -o addr show | grep tun || ip -4 -o addr show | grep eth || ip -4 -o addr show | grep enp || ip -4 -o addr show | grep wlan || ip -4 -o addr show | grep lo)
 DEV=$(echo $IP | awk '{print $2}')
 IP=$(echo $IP | awk '{print $4}')
-
-if [[ $DEV == "" ]]; then
-	DEV="localhost"
-fi
-
-if [[ $IP == "" ]]; then
-	IP="127.0.0.1"
-fi
-
 echo -n "$DEV: "
 echo -n "%{F#00ff00}$IP"
