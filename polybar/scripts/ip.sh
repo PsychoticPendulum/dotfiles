74f7e788ba3d15188702f2f32bc68b789efb4141
luks <lukas.mairock@gmail.com>
2022-11-21 07:37:58 +0100

    Added localhost as valid IP

--- polybar/scripts/ip.sh
+++ polybar/scripts/ip.sh
@@ -3,5 +3,14 @@
 IP=$(ip -4 -o addr show | grep tun || ip -4 -o addr show | grep eth || ip -4 -o addr show | grep enp || ip -4 -o addr show | grep wlan)  
 DEV=$(echo $IP | awk '{print $2}')
 IP=$(echo $IP | awk '{print $4}')
+
+if [[ $DEV == "" ]]; then
+	DEV="localhost"
+fi
+
+if [[ $IP == "" ]]; then
+	IP="127.0.0.1"
+fi
+
 echo -n "$DEV: "
 echo -n "%{F#00ff00}$IP"
