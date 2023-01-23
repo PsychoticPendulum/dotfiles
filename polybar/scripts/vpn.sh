67f102115a6f1e12c4a13feed7d014f7934de688
luks <lukas.mairock@gmail.com>
2023-01-23 11:16:49 +0100

    differentiate home vs proxy vpn

--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo -n 'VPN: '
-if ip addr | grep tun > /dev/null; then
-	echo '%{F#00ff00}Active'
-	exit
+
+if ip addr | grep -E "tun[0-9]+" > /dev/null; then
+	echo '%{F#00ff00}Up'
+elif ip addr | grep tunr > /dev/null; then
+	echo '%{F#00ff00}Home'
 else
-	echo '%{F#ff0000}Inactive'
-	# ps -aux | grep 'torrent' | awk '{print $2}' | xargs kill
+	echo '%{F#ff0000}Down'
 fi
