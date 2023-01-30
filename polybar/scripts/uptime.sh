757c8d985ff2f7698fa5dfd650ce4f4019b3d671
luks <lukas.mairock@gmail.com>
2023-01-30 10:31:27 +0100

    lengthened

--- polybar/scripts/uptime.sh
+++ polybar/scripts/uptime.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-uptime -p | sed s/" days,"/"d"/g | sed s/" day."/"d"/g | sed s/" hours,"/"h"/g | sed s/" hour,"/"h"/g | sed s/" minutes"/"m"/g | sed s/" minute"/"m"/g | sed s/"up"//g | sed s/" "//g
+uptime -p | sed s/" days,"/"d"/g | sed s/" day."/"d"/g | sed s/" hours,"/"h"/g | sed s/" hour,"/"h"/g | sed s/" minutes"/"m"/g | sed s/" minute"/"m"/g | sed s/"up "//g
