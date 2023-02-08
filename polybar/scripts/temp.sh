c85d0a9bc50d397a99e89613b0302d5b1670af85
luks <lukas.mairock@gmail.com>
2023-02-08 20:46:15 +0100

    temp sensor on balcony now

--- polybar/scripts/temp.sh
+++ polybar/scripts/temp.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/bash
 
-mosquitto_sub -C 1 -h 192.168.4.37 -t temp/inside --quiet 
+inside=$(mosquitto_sub -C 1 -h 192.168.4.37 -t temp/inside --quiet | awk '{print $1}')
+outside=$(mosquitto_sub -C 1 -h 192.168.4.37 -t temp/outside --quiet | awk '{print $1}')
+echo "$inside / $outside"
