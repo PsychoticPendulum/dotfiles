e10f78045d59370dde48bdeb0cba1da2b7370fe0
luks <lukas.mairock@gmail.com>
2023-02-09 22:16:45 +0100

    cxzdskjlfmvhn,<iou

--- polybar/scripts/temp.sh
+++ polybar/scripts/temp.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-inside=$(mosquitto_sub -C 1 -h 192.168.4.37 -t temp/inside --quiet | awk '{print $1}')
-outside=$(mosquitto_sub -C 1 -h 192.168.4.37 -t temp/outside --quiet | awk '{print $1}')
-echo "$inside / $outside"
+inside=$(mosquitto_sub -W 1 -C 1 -h 192.168.4.37 -t temp/inside --quiet | awk '{print $1}')
+outside=$(mosquitto_sub -W 1 -C 1 -h 192.168.4.37 -t temp/outside --quiet | awk '{print $1}')
+echo "$inside $outside"
