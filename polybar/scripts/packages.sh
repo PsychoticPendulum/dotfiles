c89efd67ee61b5bcaaf1c6bd707d7b68c42eccff
luks <lukas.mairock@gmail.com>
2023-02-10 15:08:44 +0100

    void integration

--- polybar/scripts/packages.sh
+++ polybar/scripts/packages.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
-TOTAL=$(xbps-query -l | wc -l)
-PENDING=$(xbps-install --memory-sync --dry-run --update | grep -Fe update -e install | wc -l)
-COLOR=""
+distro=$(lsb_release -d | awk '{print $2}')
+
+if [[ $distro =~ "Void" ]]; then
+	TOTAL=$(xbps-query -l | wc -l)
+	PENDING=$(xbps-install --memory-sync --dry-run --update | grep -Fe update -e install | wc -l)
+	COLOR=""
+else
+	TOTAL=$(pacman -Q | wc -l)
+	PENDING=$(checkupdates | wc -l)
+	COLOR=""
+fi
 
 if [[ "$PENDING" -gt "25" ]]; then
 	COLOR=" %{F#ff0000}$PENDING"
