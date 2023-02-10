e348e10eb533b97494db09a3fd21c24d3a093c6c
luks <lukas.mairock@gmail.com>
2023-02-10 14:12:08 +0100

    longer timeout period

--- polybar/scripts/temp.sh
+++ polybar/scripts/temp.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/bash
 
-inside=$(mosquitto_sub -W 1 -C 1 -h 192.168.4.37 -t temp/inside --quiet | awk '{print $1}')
-outside=$(mosquitto_sub -W 1 -C 1 -h 192.168.4.37 -t temp/outside --quiet | awk '{print $1}')
-echo "$inside $outside"
+inside=$(mosquitto_sub -W 2 -C 1 -h 192.168.4.37 -t temp/inside --quiet | awk '{print $1}')
+outside=$(mosquitto_sub -W 2 -C 1 -h 192.168.4.37 -t temp/outside --quiet | awk '{print $1}')
+
+if [[ -z "$inside$outside" ]]; then
+	echo "..."
+else
+	echo "$inside $outside"
+fi
