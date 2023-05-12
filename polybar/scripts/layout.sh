07626071c809e103a9ece13a06f2abd555f964c4
luks <lukas.mairock@gmail.com>
2023-05-12 16:45:30 +0200

    telegram desktop app

--- polybar/scripts/layout.sh
+++ polybar/scripts/layout.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-setxkbmap -query | tail -n 1 | awk '{print $2}'
+language=$(setxkbmap -query | tail -n 1 | awk '{print $2}')
+xrate=$(xset -q | head -n 9 | tail -n 1 | awk '{print $4,$7}')
+echo $language $xrate
