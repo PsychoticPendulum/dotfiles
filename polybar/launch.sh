93b4d74b978c99187448e9a815fc8e996ced3793
Lukas Mairock <lukas.mairock@gmail.com>
2023-07-13 19:45:14 +0200

    only on one scrreen

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,16 +2,16 @@
 
 kill $(ps -aux | grep "polybar --reload bar1" | awk '{print $2}')
 
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload bar1 &
-	done
-else
-	polybar --reload bar1 &
-fi
+# if type "xrandr"; then
+# 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+# 		MONITOR=$m polybar --reload bar1 &
+# 	done
+# else
+# 	polybar --reload bar1 &
+# fi
 
 ##  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-# polybar bar1 >> /tmp/polybar1.log 2>&1 &
+polybar bar1 >> /tmp/polybar1.log 2>&1 &
 # polybar bar2 >> /tmp/polybar2.log 2>&1 &
 
 echo "Bars launched ..."
