4b4c7dcbe29ba34525ccb8511439d965edf06741
luks <lukas.mairock@gmail.com>
2023-10-19 08:58:59 +0200

    pllybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,6 @@
 #!usr/bin/env bash
 
-ps -aux | grep polybar | awk '{print $2}' | xargs kill
+# ps -aux | grep polybar | awk '{print $2}' | xargs kill
 
 # if type "xrandr"; then
 # 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
@@ -11,7 +11,8 @@ ps -aux | grep polybar | awk '{print $2}' | xargs kill
 # fi
 
 ##  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >> /tmp/polybar1.log 2>&1 &
+# polybar bar1 >> /tmp/polybar1.log 2>&1 &
 # polybar bar2 >> /tmp/polybar2.log 2>&1 &
+polybar bar1
 
 echo "Bars launched ..."
