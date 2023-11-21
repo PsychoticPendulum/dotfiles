95dd3d80cf3ff5dd6e39be4012ab0207ef0f018c
luks <lukas.mairock@gmail.com>
2023-11-21 17:56:45 +0100

    updated

--- polybar/scripts/layout.sh
+++ polybar/scripts/layout.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-language=$(setxkbmap -query | tail -n 1 | awk '{print $2}')
+language=$(setxkbmap -query | tail -n 2 | head -n 1 | awk '{print $2}')
 xrate=$(xset -q | head -n 9 | tail -n 1 | awk '{print $4,$7}')
 echo $language $xrate
