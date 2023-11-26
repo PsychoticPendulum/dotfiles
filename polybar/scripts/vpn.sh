9e52ce8a04a8591c5f06d7f941dceeb0d22e782f
luks <lukas.mairock@gmail.com>
2023-11-26 20:30:18 +0100

    show vpn country name

--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 echo -n 'VPN: '
 
+COUNTRY=$(nordvpn status | head -n 4 | tail -n 1 | awk '{print $2}')
+
 if ip addr | grep -E "(nord|tun)" > /dev/null; then
-	echo '%{F#00ff00}Up'
+	echo "%{F#00ff00}$COUNTRY"
 elif ip addr | grep tunr > /dev/null; then
 	echo '%{F#00ff00}Home'
 else
