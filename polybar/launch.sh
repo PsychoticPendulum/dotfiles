314ffa5c3d1f35a840b09b6479f6fd87de51c3d0
luks <lukas.mairock@gmail.com>
2024-01-11 16:31:32 +0100

    polybar kills itself on relaunch

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,18 +1,6 @@
-#!usr/bin/env bash
+#! usr/bin/bash
 
-# ps -aux | grep polybar | awk '{print $2}' | xargs kill
-
-# if type "xrandr"; then
-# 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-# 		MONITOR=$m polybar --reload bar1 &
-# 	done
-# else
-# 	polybar --reload bar1 &
-# fi
-
-##  echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-# polybar bar1 >> /tmp/polybar1.log 2>&1 &
-# polybar bar2 >> /tmp/polybar2.log 2>&1 &
+killall -9 polybar
 polybar bar1
 
 echo "Bars launched ..."
