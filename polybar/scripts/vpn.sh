5d4ca3d90ec353602b03412593afbf9a56f1a9c1
luks <lukas.mairock@gmail.com>
2024-02-08 20:14:59 +0100

    >_

--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -2,9 +2,10 @@
 echo -n 'VPN: '
 
 CITY=$(nordvpn status | grep "City" | awk '{print $2}')
+DATA=$(nordvpn status | grep "Transfer" | awk '{print $2$3, $5$6}')
 
 if ip addr | grep -E "(nord|tun)" > /dev/null; then
-	echo "%{F#00ff00}$CITY"
+	echo "%{F#00ff00}$CITY ($DATA)"
 elif ip addr | grep tunr > /dev/null; then
 	echo '%{F#00ff00}Home'
 else
